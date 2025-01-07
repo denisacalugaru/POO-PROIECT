5e6a04b5ace4812ac5781b0ee5f8d4511efe3125
denisacalugaru <calugarud55@gmail.com>
2025-01-07 20:18:41 +0200

    43

--- cpp/Menu.cpp
+++ cpp/Menu.cpp
@@ -41,19 +41,19 @@ void Menu::print() const {
         category.print();
     }
 }
-[[maybe_unused]]void Menu::checkAvailability(const std::string& time) {
+void Menu::checkAvailability(const std::string& time) {
     if (time != "08:00" && time != "12:00" && time != "18:00") {
         throw InvalidTimeException("Invalid time for menu availability.");
     }
 }
 
-[[maybe_unused]]void Menu::checkMenuEmpty(const std::vector<std::string>& menuItems) {
+void Menu::checkMenuEmpty(const std::vector<std::string>& menuItems) {
     if (menuItems.empty()) {
         throw EmptyMenuException("The menu is empty.");
     }
 }
 
-[[maybe_unused]]void Menu::checkDishValidity(const std::string& dish) {
+void Menu::checkDishValidity(const std::string& dish) {
     if (dish != "Pizza" && dish != "Pasta" && dish != "Burger") {
         throw InvalidDishException("The specified dish does not exist.");
     }
